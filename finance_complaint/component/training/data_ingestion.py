a858a3711a52054f8cea11c3c6ce4f970237bd85
AvinashGirme <girmeavinash717@gmail.com>
2022-12-14 13:05:55 +0530

    updated2

--- finance_complaint/component/training/data_ingestion.py
+++ finance_complaint/component/training/data_ingestion.py
@@ -16,4 +16,28 @@ from finance_complaint.entity.metadata_entity import DataIngestionMetadata
 from finance_complaint.exception import FinanceException
 from finance_complaint.logger import logger
 from datetime import datetime
+from finance_complaint.config.spark_manager import spark_session
+
+DownloadUrl=namedtuple("DownloadUrl", ["url","file_path","n_retry"])
+
+class DataIngestion:
+    #used to download data in chunks.
+    def __init__(self,data_ingestion_config:DataIngestionConfig,n_retry: int =5,):
+        """
+        data_ingestion_config: Data Ingestion Config
+        n_retry: Number of retry filed should be tried to download in case of failure encountered
+        n_month_interval: n month data will be downloaded 
+        """
+
+        try:
+            logger.info(f"{'>>' * 20} Starting Data Ingestion {'<<' * 20}")
+            self.data_ingestion_config=data_ingestion_config
+            self.failed_download_urls: List[DownloadUrl] = []
+            self.n_retry=n_retry
+            
+        except Exception as e:
+            raise FinanceException(e, sys)
+            
+
+
 
--- main.py
+++ main.py
@@ -6,8 +6,11 @@ access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY,)
 secret_access_key =os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY,)
 
 import argparse
+from finance_complaint.pipeline.training import TrainingPipeline
 from finance_complaint.exception import FinanceException
 from finance_complaint.logger import logger
+from finance_complaint.config.pipeline.training import FinanceConfig
+
 
 
 def start_training(start=False):
@@ -15,6 +18,28 @@ def start_training(start=False):
         if not start:
             return None
         print("Training Running")
+        TrainingPipeline(finance_config()).start()
     
     except Exception as e:
         raise FinanceException(e, sys)
+
+
+def main(training_status):
+    try:
+        start_training(start=training_status)
+    except Exception as e:
+        raise FinanceException(e, sys)
+
+if __name__=="__main__":
+    try:
+        parser=argparse.ArgumentParser()
+        parser.add_argument("--t",default=0,type=int,help="if provided true training will be done else not")
+
+        args=parser.parse_args()
+        main(training_status=args.t)
+
+    except Exception as e:
+        print(e)
+        pass
+        logger.exception(FinanceException(e,sys))
+        
