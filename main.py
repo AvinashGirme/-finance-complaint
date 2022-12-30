c06234093657cf1eb84f6ebefc83926ecd1dffb9
AvinashGirme <girmeavinash717@gmail.com>
2022-12-30 19:24:43 +0530

    main updated

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ access_key_id = os.getenv(AWS_ACCESS_KEY_ID_ENV_KEY,)
 secret_access_key =os.getenv(AWS_SECRET_ACCESS_KEY_ENV_KEY,)
 
 import argparse
-from finance_complaint.pipeline import TrainingPipeline
+from finance_complaint.pipeline import TrainingPipeline,PredictionPipeline
 from finance_complaint.exception import FinanceException
 from finance_complaint.logger import logger
 from finance_complaint.config.pipeline.training import FinanceConfig
@@ -23,21 +23,34 @@ def start_training(start=False):
     except Exception as e:
         raise FinanceException(e, sys)
 
+def start_prediction(start=False):
+    try:
+        if not start:
+            return None
+        print("Prediction Started")
+        PredictionPipeline().start_batch_prediction()
+    except Exception as e:
+        raise FinanceException(e, sys)
 
-def main(training_status):
+def main(training_status,prediction_status):
     try:
         start_training(start=training_status)
+        start_prediction(start=prediction_status)
         
     except Exception as e:
         raise FinanceException(e, sys)
+
+
 
 if __name__=="__main__":
     try:
         parser=argparse.ArgumentParser()
         parser.add_argument("--t",default=0,type=int,help="if provided true training will be done else not")
+        parser.add_argument("--p",default=0,type=int,help="if provided prediction will be done else not")
 
         args=parser.parse_args()
-        main(training_status=args.t)
+        main(training_status=args.t,prediction_status=args.p)
+        
 
     except Exception as e:
         print(e)
